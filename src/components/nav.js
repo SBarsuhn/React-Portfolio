b7f693dc98107a1e862e7b4be150fef11e65d212
SBars <samuelbarsuhn@gmail.com>
2023-05-09 20:46:03 -0500

    added highlight for the navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link, NavLink, useLocation} from 'react-router-dom'
 const Nav = () => {
+  let location = useLocation()
+  console.log(location)
   return (
     <div className="navbar">
      <ul>
-       <li><Link className="navbar" to = '/'>ABOUT ME</Link></li>
-       <li><Link className="navbar" to = '/portfolio'>PORTFOLIO</Link></li>
-       <li><Link className="navbar" to = '/contact'>CONTACT</Link></li>
-       <li><Link className="navbar" to = '/resume'>RESUME</Link></li>
+       <li><NavLink className={location.pathname === "/" ? "highlight" : "navbar"} to = '/'>ABOUT ME</NavLink></li>
+       <li><NavLink className={location.pathname === "/portfolio" ? "highlight" : "navbar"} to = '/portfolio'>PORTFOLIO</NavLink></li>
+       <li><NavLink className={location.pathname === "/contact" ? "highlight" : "navbar"} to = '/contact'>CONTACT</NavLink></li>
+       <li><NavLink className={location.pathname === "/resume" ? "highlight" : "navbar"} to = '/resume'>RESUME</NavLink></li>
      </ul>
     </div>
   );
