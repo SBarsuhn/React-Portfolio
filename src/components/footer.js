42fd9af4f4a1559d7efe70aed7a59855bb854a9d
SBars <samuelbarsuhn@gmail.com>
2023-05-09 21:13:41 -0500

    made some changes to the portfolio page and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import github from '../images/github.png'
+import linkedin from '../images/linkedin-logo.png'
+import twitter from '../images/twitter.png'
 const Footer = () => {
   return (
     <div className="item">
-      <div>
-     <a target='_blank' href='https://github.com/SBarsuhn'>put github</a>
-     <a>put linkedin</a>
-     <a>put twitter</a>
+      <div className='footer-cont'>
+     <a target='_blank' href='https://github.com/SBarsuhn'><img className='footer-img' src={github} /></a>
+     <a target='_blank' href='https://linkedin.com'><img className='footer-img' src={linkedin} /></a>
+     <a target='_blank' href='https://twitter.com'><img className='footer-img' src={twitter} /></a>
     </div>
     </div>
   );
--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Project = (props) => {
   return (
     <div className="item">
-      <div className='cards-cont'>
-     <div className='card'>
-      <div className='card-title'>{props.title}</div>
-      <img src={props.image} className='card-image' />
-      <div className='card-desc'>{props.description}</div>
-      <a target="blank" href={props.url} className='card-url'>
-      <button className='site-but'>VISIT SITE</button>
-      </a>
-     </div>
-    </div>
+      <div className="cards-cont">
+        <div className="card">
+          <div className="card-title">{props.title}</div>
+          <img src={props.image} className="card-image" />
+          <div className="card-desc">{props.description}</div>
+          <a target="blank" href={props.url} className="card-url">
+            <button className="site-but">VISIT SITE</button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
