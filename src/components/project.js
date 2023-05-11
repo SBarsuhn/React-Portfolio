8ef6125c795027db39dbc660c23b3761f76f5b63
SBars <samuelbarsuhn@gmail.com>
2023-05-11 16:47:45 -0500

    finished the portfolio and resume sections

--- src/components/project.js
+++ src/components/project.js
@@ -7,10 +7,10 @@ const Project = (props) => {
         <div className="card">
           <div className="card-title">{props.title}</div>
           <img src={props.image} className="card-image" />
-          <div className="card-desc">{props.description}</div>
           <a target="blank" href={props.url} className="card-url">
             <button className="site-but">VISIT SITE</button>
           </a>
+          <div className="card-desc">{props.description}</div>
         </div>
       </div>
     </div>
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import image from '../images/Broker-exchange-image.png'
+import image from '../images/Rectangle@2x-2-3.png'
 
 const Resume = () => {
   return (
     <div className="item">
-      <a href={image} download>DOWNLOAD HERE</a>
+      <a href={image} download><button className='download'>DOWNLOAD</button></a>
+      <div className='res-cont'>
+      <img className='resume' src={image} alt='resume' />
+    </div>
+      <span>This is just an example. When I have an actual resume I will replace this one</span>
     </div>
   );
 };
